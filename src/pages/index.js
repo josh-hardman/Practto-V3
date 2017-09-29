9f85785d0e046a7d66ac005fd0db37ba34d355e1
Joshua Hardman <jhardman0830@gmail.com>
2017-09-28 23:37:02 -0600

    Adding filters to landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,11 @@ import styled from "styled-components";
 import { toRem } from "../utils/utils";
 import theme from "../theme/theme";
 import Section from "../layouts/Section";
-// import Typist from "react-typist";
-
+import Typing from "react-typing-animation";
+import Card from "../components/Card";
+import RaisedButton from "material-ui/RaisedButton";
+import AutoComplete from "material-ui/AutoComplete";
+import { gql, graphql } from "react-apollo";
 // const Background = styled.div`
 //   background: ${theme.mediumBlue};
 //   position: fixed;
@@ -22,10 +25,31 @@ const Lede = styled.h1`
   font-weight: normal;
 `;
 
+const PracticeType = styled.h1`
+  width: 100%;
+  font-size: ${toRem(40)};
+  text-align: center;
+  color: ${theme.darkBlue};
+  font-weight: normal;
+`;
+
+const Question = styled.p`
+  color: ${theme.white};
+  font-weight: lighter;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  padding-top: 32px;
+  padding-bottom: 120px;
+`;
+
 class Landing extends Component {
   state = {
     words: ["Dentist", "Chiropractor", "Lawyer"],
-    index: 0
+    index: 0,
+    type: "Hello WORLD"
   };
 
   onHeaderTyped = () => {
@@ -33,39 +57,92 @@ class Landing extends Component {
     this.setState({
       index: 1
     });
-    this.forceUpdate();
   };
 
+  handleGetPracticeTypes = () =>
+    this.props.data.allPracticeTypeses
+      ? this.props.data.allPracticeTypeses.map(item => item.name)
+      : [];
+
+  handleGetLocation = () =>
+    this.props.data.allPractices
+      ? this.props.data.allPractices.map(
+          item => `${item.city.name}, ${item.state.name}`
+        )
+      : [];
+
+  handleGetInsurances = () =>
+    this.props.data.allInsurances
+      ? this.props.data.allInsurances.map(item => item.name)
+      : [];
+
   render() {
+    const { data } = this.props;
     return (
       <div>
         <Section background={theme.lightBlue}>
-          {/* <Background /> */}
           <Lede>Find Your Family</Lede>
-          {/* <Typist
-            className="TypistExample-message"
-            delayGenerator={this.delayGen}
-            cursor={{ hideWhenDone: true }}
-          >
-            * Easy to style
-            <Typist.Delay ms={1250} />
-            <br />
-            * Easy to customize
-            <Typist.Delay ms={1250} />
-            <br />
-            * Easy to use backspace
-            <Typist.Backspace count={23} delay={750} />
-            <span>
-              <a href={"test"} className="flash">
-                docs
-              </a>
-            </span>
-            <br />
-          </Typist> */}
+          <PracticeType>Dentist</PracticeType>
+        </Section>
+        <Section background={theme.mediumBlue} zIndex={-2} squareBottom>
+          <Question>What can we help you find today?</Question>
+          <Card background={theme.aliceBlue}>
+            <AutoComplete
+              floatingLabelFixed={true}
+              floatingLabelText="Practice Type"
+              filter={AutoComplete.fuzzyFilter}
+              dataSource={this.handleGetPracticeTypes()}
+              maxSearchResults={5}
+              disabled={!this.handleGetPracticeTypes().length}
+            />
+            <AutoComplete
+              floatingLabelFixed={true}
+              floatingLabelText="City"
+              filter={AutoComplete.fuzzyFilter}
+              dataSource={this.handleGetLocation()}
+              maxSearchResults={5}
+              disabled={!this.handleGetLocation().length}
+            />
+            <AutoComplete
+              floatingLabelFixed={true}
+              floatingLabelText="Insurance Provider"
+              filter={AutoComplete.fuzzyFilter}
+              dataSource={this.handleGetInsurances()}
+              maxSearchResults={5}
+              disabled={!this.handleGetInsurances().length}
+            />
+          </Card>
+          <ButtonContainer>
+            <RaisedButton
+              label="Search"
+              labelStyle={{ textTransform: "none", fontWeight: "lighter" }}
+              labelColor={theme.white}
+              backgroundColor={theme.orange}
+            />
+          </ButtonContainer>
         </Section>
       </div>
     );
   }
 }
 
-export default Landing;
+const Query = gql`
+  query {
+    allPracticeTypeses {
+      name
+    }
+    allPractices {
+      city {
+        name
+      }
+      state {
+        name
+      }
+    }
+    allInsurances {
+      name
+    }
+  }
+`;
+
+export default graphql(Query)(Landing);
