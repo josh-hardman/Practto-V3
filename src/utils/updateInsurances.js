28a05269c1057f1b4c788ee0cfa89ef1bc8798a8
Joshua Hardman <jhardman0830@gmail.com>
2018-01-04 20:31:12 -0700

    adding update insurances

---
+++ src/utils/updateInsurances.js
@@ -0,0 +1,68 @@
+mutation($id: ID!) {
+  updatePractice(
+    id: $id
+    insurancesIds: ["cj7po2xm3aq8k0115pfstdndd","cj7po34zma7tb01724rbb4rzo","cj7po3g3ybfle0153j3k0cq85","cj7po3mbnbft70153zhuisanp","cj7po4splanvq01059avle43a","cj7po500vapfl0135fiicw60n","cj7po55e3at5f0115aydf2jff","cj7po5a51atag0115um2p1gws","cj7po5eoeaakp0172tzdifhxp","cj7po5iyeaovg0105hixkhm6f","cj7po5o7laayd01728r7owo3y","cj7po5t43bitt0153bkkadu0g","cj7po5x2wabdl0172o4gy5d83","cj7po61moav9r01480wlp7h7k","cj7po65yvb7pk0150v6owdevi","cj7po6ab0cddx01858s8fad9s","cj7po6e8ubjp40153t7j03c1m","cj7po6i88ac1o0172krk805pd","cj7po6mmiaqpy0105hgngx88l","cj7po6qsvb8r5015028x41t6f","cj7po6va0bkgw0117irf7l342","cj7po73tbavzy0115dgkefz6j","cj7po77igceo80185yrjvwvye","cj7po7bn4c105017317zt6m4h","cj7po7gfubl9m0153vuu5ukdp","cj7po7jgzatmt0135vb4fqsh1","cj7po7o5jattk0135ykxtk16j","cj7po7slqbayy0150fu4owge6","cj7po7wibbm6f01533k2yg4su","cj7po8121bmff0153oravfscj","cj7po85gfaerb01727zrlik8h","cj7po89etattu01057z0ft0xz","cj7po8dj7atxw01054tjdxwpp","cj7po8h6dc2wc0173ddod6i7b","cj7po8lmbav5m0135vg3njwpv","cj7po8pe7ax88013600rqt0zq","cj7po8tr1c3bj0173ntvhthic","cj7po8z9jaus3010598n8qdo4","cj7po940jaw0o01350ko6nkf0","cj7po982tbo5401179fjw3p4b","cj7po9c84av8m0105ogzafw93","cj7po9g7kagjx0172ly8lwt9y","cj7po9knhawlt0135cm6q4dqx","cj7po9uhybp6c0153tzsibdrh","cj7po9z18az6e0136wvv3sjda","cj7poa9tubq420153vu9vt2eb","cj7poag3ebesc01506mlsplq7","cj7poakgkb13m0115lakpsrwk","cj7poaoafbqr10117op2rivih","cj7poasmfb3fq0148c7hie3bf","cj7poax6qay3e01053qenamn2","cj7pob1o9aydo010599qbavcu","cj7pob6dfb1lu0136d1qw3h9l","cj7pobaowayrf0105nfsv0xr9","cj7pobf23b4590148lu8ooin3"]
+  ) {
+    id
+    name
+		insurances {
+		  id
+		}
+  }
+}
+
+PracticeIDs: 
+
+"cj8azcid0eyvd0172ydbebiyf"
+"cj8azr4crgw1s0179337cfaat"
+"cj8azxei9ph0j0132klu0mg2p"
+"cj8b07rxvfq4o0177fow8p1k2"
+"cj8b0b7gticib0139na2y710y"
+"cj8b0uytqqmxd0132w1l2u5ed"
+"cj8b16riqhauf0172t4osnh7h"
+"cj8b1bmllhd110108604pswa7"
+"cj8b1pnl2i1bl0177gho9gnda"
+"cj8c6itluuo1y0179oie5q20p"
+"cj8c6nasjure30122rf4ihpsh"
+"cj8c6zca5v0cg0160g4vc2qwf"
+"cj8c74d22veag019078h1iejo"
+"cj8c7bj46vskb01548874oa5b"
+"cj8c7jjfzw8n00149vjjri7u7"
+"cj8c7t5nbwrm501865eow9t3w"
+"cj8c7z56rwohd01793n9w9izm"
+"cj8c821r4wm3t0109hwxtd45g"
+"cj8c8ltzsxu310100vvypxhr2"
+"cj8c8ykq6xxpt0160hieylu57"
+"cj8c96yhvy27s01093hzdt192"
+"cj8jkzdzxjdsv0110f6yddnup"
+"cj8kmtpv3v82k0120i4r0wrsh"
+"cj8kn6a5wqdox0145fop3uh3y"
+"cj8kne4d4q0g60148pz7s0lsr"
+"cj8qds4wtheuk0141tj8is6ay"
+"cj8qdvnvlhqcm0136lb9gb39k"
+"cj8qe3lm0i6tl0136fd0uhg2l"
+"cj8qeauyhifgp01412tp9258d"
+"cj8qeq42oj4ed0141yp6wmg34"
+"cj8qewr8fjsle0179rcgfbzih"
+"cj8ta11v3rui00178dya0ucs7"
+"cj8ta71x6rody01044f7sibjk"
+"cj8tadoymrynm0129z6ldd1k2"
+"cj8tajafvsf7501278b2pjqqg"
+"cj8tark1bsis201816wk55hfc"
+"cj8tb58wktdxl0130a3omfnbl"
+"cj8tc132nuoof0130mqm7xxb1"
+"cj8tc6iufuql60104fft8nigz"
+"cj8tcn1i7vbhh0157ghalpzp3"
+"cj8tcwl48w2nh01272i17a1a6"
+"cj8td4w9swc9l015774s12twc"
+"cj8tdfr6zxcg20178fld99qyi"
+"cj8tdpi9cx0vo0170y8616hqc"
+"cj8te4nw5y4l901270nv7u5nq"
+"cj8tebd4ey7kd0129kcmou5mk"
+"cj8tpflrdfab70104d5tjqs7p"
+"cj8tpspi0fvld0104p9r97drq"
+"cj8uf7j1l1zr00105wlx34bht"
+"cj94r201p5yra0153z1a11m7z"
+
+Insurance Ids:
+"cj7po2xm3aq8k0115pfstdndd","cj7po34zma7tb01724rbb4rzo","cj7po3g3ybfle0153j3k0cq85","cj7po3mbnbft70153zhuisanp","cj7po4splanvq01059avle43a","cj7po500vapfl0135fiicw60n","cj7po55e3at5f0115aydf2jff","cj7po5a51atag0115um2p1gws","cj7po5eoeaakp0172tzdifhxp","cj7po5iyeaovg0105hixkhm6f","cj7po5o7laayd01728r7owo3y","cj7po5t43bitt0153bkkadu0g","cj7po5x2wabdl0172o4gy5d83","cj7po61moav9r01480wlp7h7k","cj7po65yvb7pk0150v6owdevi","cj7po6ab0cddx01858s8fad9s","cj7po6e8ubjp40153t7j03c1m","cj7po6i88ac1o0172krk805pd","cj7po6mmiaqpy0105hgngx88l","cj7po6qsvb8r5015028x41t6f","cj7po6va0bkgw0117irf7l342","cj7po73tbavzy0115dgkefz6j","cj7po77igceo80185yrjvwvye","cj7po7bn4c105017317zt6m4h","cj7po7gfubl9m0153vuu5ukdp","cj7po7jgzatmt0135vb4fqsh1","cj7po7o5jattk0135ykxtk16j","cj7po7slqbayy0150fu4owge6","cj7po7wibbm6f01533k2yg4su","cj7po8121bmff0153oravfscj","cj7po85gfaerb01727zrlik8h","cj7po89etattu01057z0ft0xz","cj7po8dj7atxw01054tjdxwpp","cj7po8h6dc2wc0173ddod6i7b","cj7po8lmbav5m0135vg3njwpv","cj7po8pe7ax88013600rqt0zq","cj7po8tr1c3bj0173ntvhthic","cj7po8z9jaus3010598n8qdo4","cj7po940jaw0o01350ko6nkf0","cj7po982tbo5401179fjw3p4b","cj7po9c84av8m0105ogzafw93","cj7po9g7kagjx0172ly8lwt9y","cj7po9knhawlt0135cm6q4dqx","cj7po9uhybp6c0153tzsibdrh","cj7po9z18az6e0136wvv3sjda","cj7poa9tubq420153vu9vt2eb","cj7poag3ebesc01506mlsplq7","cj7poakgkb13m0115lakpsrwk","cj7poaoafbqr10117op2rivih","cj7poasmfb3fq0148c7hie3bf","cj7poax6qay3e01053qenamn2","cj7pob1o9aydo010599qbavcu","cj7pob6dfb1lu0136d1qw3h9l","cj7pobaowayrf0105nfsv0xr9","cj7pobf23b4590148lu8ooin3"
