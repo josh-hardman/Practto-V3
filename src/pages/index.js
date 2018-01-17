651bae38fac15004bde64d8c4b06630f29f19157
Joshua Hardman <jhardman0830@gmail.com>
2018-01-16 20:19:15 -0700

    updating text

--- src/pages/index.js
+++ src/pages/index.js
@@ -132,7 +132,7 @@ class Landing extends Component {
         >
           <Backdrop>
             <Background src="landing.png" />
-            <Lede>Dentto lists the highest rated Dentists near you!</Lede>
+            <Lede>Dentto connects you with a Dentist you can trust!</Lede>
           </Backdrop>
           <Card background={theme.aliceBlue}>
             <FilterContainer>
